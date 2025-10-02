42711825bac6f845a6d3b649eab3ebea044b76d3
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-10-02 20:11:22 +0530

    init

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,15 +1,56 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+
 const Topbar = () => {
   return (
-    <div
-      className="flex items-center justify-between p-4 sticky top-0 bg-yellow-50
-       z-10 
-    "
+    <nav
+      className="flex items-center justify-between px-6 py-3 sticky top-0 
+      bg-white/80 backdrop-blur-md shadow-md rounded-2xl w-full max-w-screen-xl mx-auto mt-2 z-20"
     >
-      <div className="flex gap-2 items-center">
-        <img src="/AutoInsight.png" className="size-12" alt="A" />
+      {/* Logo Section */}
+      <div className="flex items-center gap-2">
+        <img
+          src="/AutoInsight.png"
+          className="h-10 w-10 rounded-lg"
+          alt="AutoInsight Logo"
+        />
+        <span className="text-xl font-semibold text-gray-800 hidden sm:block">
+          AutoInsight
+        </span>
       </div>
-      <div className="flex items-center gap-4"></div>
-    </div>
+
+      {/* Right Section */}
+      <div className="flex items-center gap-4">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              className="bg-gray-300 hover:bg-white/100 
+             text-gray-800 font-medium 
+             rounded-xl px-5 py-2.5 shadow-md backdrop-blur-sm transition 
+             duration-200"
+            >
+              EXPORT
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+            align="end"
+            className="w-40 rounded-xl shadow-md"
+          >
+            <DropdownMenuLabel>Format</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>CSV</DropdownMenuItem>
+            <DropdownMenuItem>JSON</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </nav>
   );
 };
 
---
+++ frontend/src/pages/footer/footer.tsx
@@ -0,0 +1,5 @@
+const footer = () => {
+  return <div>Footer</div>;
+};
+
+export default footer;
